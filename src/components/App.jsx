f1f43122d393dde9ff71c5cc412539549a0f1c3f
Okedo01 <eliahmwelangi01@gmail.com>
2025-05-20 17:13:03 +0300

    todo-app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,12 @@
 import Todo from "./todo"
 import bgImg from "../assets/images/bg-desktop-dark.jpg"
+import "../App.css"
 
 function App() {
 
   return (
      <div style={{
       backgroundImage: `url(${bgImg})`,
-      margin: 0,
-      padding: 0,
-      boxSizing: "border-box",
-      height: "200px",
-      display: "grid",
-      placeItems: "center",
     }}>
        <Todo />
     </div>
